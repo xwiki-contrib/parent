669ef9cb5ebe51130564a0d16bc67b7f435e6fc7
Thomas Mortagne <thomas.mortagne@gmail.com>
2025-08-01 10:24:04 +0200

    Try to use xmvn instead of mvn if it's available

--- release.sh
+++ release.sh
@@ -96,7 +96,11 @@ function deploy_pom() {
   cd $1
 
   echo -e "\033[0;32m* Deploy ${1} ${VERSION}\033[0m"
-  mvn clean deploy -DperformRelease=true -Prelease-parent
+  if command -v xmvn &> /dev/null; then
+    xmvn clean deploy -DperformRelease=true -Prelease-parent
+  else
+    mvn clean deploy -DperformRelease=true -Prelease-parent
+  fi
 
   cd ..
 }
